dd0190402a017815688ea289e8b62cbd36a1645b
2222-42 <stephen0617@me.com>
2021-02-23 06:40:36 +0900

    feat(chap3): clear screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::{event::Key, input::TermRead, raw::IntoRawMode};
 
 pub struct Editor {
@@ -10,18 +10,27 @@ impl Editor {
         let _stdout = stdout().into_raw_mode().unwrap();
 
         loop {
-            if let Err(error) = self.process_keypress() {
+            if let Err(error) = self.refresh_screen() {
                 die(error);
             }
 
             if self.should_quit {
                 break;
             }
+
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
         }
     }
 
     pub fn default() -> Self {
         Editor { should_quit: false }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        print!("{}", termion::clear::All);
+        io::stdout().flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
