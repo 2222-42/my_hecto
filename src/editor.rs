1e31068933f0381dd7c899d39784f36ee5ba1618
2222-42 <stephen0617@me.com>
2021-03-04 06:28:15 +0900

    chore(chap4): add row and document

--- src/editor.rs
+++ src/editor.rs
@@ -1,9 +1,10 @@
-use crate::Terminal;
+use crate::{Document, Terminal};
 
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -12,6 +13,7 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    document: Document,
 }
 
 impl Editor {
@@ -35,13 +37,14 @@ impl Editor {
         Editor {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal."),
-            cursor_position: Position { x: 0, y: 0 },
+            cursor_position: Position::default(),
+            document: Document::default(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_postion(&Position { x: 0, y: 0 });
+        Terminal::cursor_postion(&Position::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
